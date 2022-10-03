1e10e8841ce4e5c85ea9588dc4f2ef6e88f81d89
Yanzen <yanwarsolah@gmail.com>
2022-10-03 13:42:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate, Outlet, Link } from "react-router-dom";
+
+const Confirm = () => {
+  return (
+    <>
+      <h1>Kamu belum login!</h1>
+      <p>Untuk login, klik <Link to="/login">login</Link></p>
+    </>
+  )
+}
+
+const AuthGuard = () => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    if (!token) {
+      setIsAuthenticated(false)
+    } else {
+      setIsAuthenticated(true)
+    }
+  }, [])
+
+  return (isAuthenticated ? <Outlet />: <Confirm/>)
+}
+
+const Dashboard = () => {
+  return <h1>Dashboard</h1>
+}
+
+const Login = () => {
+  return <h1>Login</h1>
+}
+
+const Profile = () => {
+  return <h1>Profile</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthGuard />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
